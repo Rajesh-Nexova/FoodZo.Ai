a197f47d8963fddeea13aa6a14f3f1ae28e13eb9
Rajesh-Nexova <rajeshnexova@gmail.com>
2025-05-19 15:53:00 +0530

    Add files via upload

---
+++ 2f514cd9f61eec56b1d4f24167823414.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-window-sidebar" viewBox="0 0 16 16" role="img" path="bs.window-sidebar" componentName="orchid-icon">
+  <path d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"></path>
+  <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v2H1V3a1 1 0 0 1 1-1h12zM1 13V6h4v8H2a1 1 0 0 1-1-1zm5 1V6h9v7a1 1 0 0 1-1 1H6z"></path>
+</svg>
+<?php /**PATH /Users/george/Dropbox/code/dine-hub/backend/storage/framework/views/edacc11fdc2be575d18281c30694c4bc.blade.php ENDPATH**/ ?>
