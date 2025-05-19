f81d23efd058a19346d1f04196a8e41ebe784416
Rajesh-Nexova <rajeshnexova@gmail.com>
2025-05-19 16:34:17 +0530

    Delete 2bd4691e3b98d54805ea6e20e4a7ce3d.php

--- 2bd4691e3b98d54805ea6e20e4a7ce3d.php
+++
@@ -1,5 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="me-2" viewBox="0 0 16 16" role="img" path="bs.tag" componentName="orchid-icon">
-  <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z"></path>
-  <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z"></path>
-</svg>
-<?php /**PATH /Users/george/Dropbox/code/dine-hub/backend/storage/framework/views/f297fcbd7d6b69c726a537f7a59e9202.blade.php ENDPATH**/ ?>
