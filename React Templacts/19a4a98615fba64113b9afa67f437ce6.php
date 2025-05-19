fd44b838e345a3be6c7ef394e2e2cdb178d7d253
Rajesh-Nexova <rajeshnexova@gmail.com>
2025-05-19 16:58:04 +0530

    Add files via upload

---
+++ React Templacts/19a4a98615fba64113b9afa67f437ce6.php
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="me-2" viewBox="0 0 16 16" role="img" path="bs.bar-chart" componentName="orchid-icon">
+  <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"></path>
+</svg>
+<?php /**PATH /Users/george/Dropbox/code/dine-hub/backend/storage/framework/views/de1ffbfbb995d91c1e7185755ce5687c.blade.php ENDPATH**/ ?>
---
+++ React Templacts/8a1aa3932d1cc552e750672de3e91413.php
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="me-2" viewBox="0 0 16 16" role="img" path="bs.list" componentName="orchid-icon">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
+</svg>
+<?php /**PATH /Users/george/Dropbox/code/dine-hub/backend/storage/framework/views/ab55942e981c5f93ceb79d4fd9b703b8.blade.php ENDPATH**/ ?>
