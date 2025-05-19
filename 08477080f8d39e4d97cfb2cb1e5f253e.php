40e80d307b251d9b11ec3abb95310958925b1f86
Rajesh-Nexova <rajeshnexova@gmail.com>
2025-05-19 16:33:22 +0530

    Delete 08477080f8d39e4d97cfb2cb1e5f253e.php

--- 08477080f8d39e4d97cfb2cb1e5f253e.php
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="me-2" viewBox="0 0 16 16" role="img" path="bs.percent" componentName="orchid-icon">
-  <path d="M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0zM4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"></path>
-</svg>
-<?php /**PATH /Users/george/Dropbox/code/dine-hub/backend/storage/framework/views/4d36f7a09021c6261f030f470538ad70.blade.php ENDPATH**/ ?>
