6fbd6728f41311419a447edeef8962735ad06f1d
Rajesh-Nexova <rajesh.r@nexovaglobaltechnology.com>
2025-05-19 16:52:49 +0530

    deleted all php files

--- 21e5e63379ea37160f203b4069474c4f.blade.php
+++
@@ -1,3 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="icon-menu" viewBox="0 0 16 16" role="img" path="bs.three-dots-vertical" componentName="orchid-icon">
-  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
-</svg>
--- 28d03f51878c48ac28f5ae4ebe647509.php
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="me-2" viewBox="0 0 16 16" role="img" path="bs.x" componentName="orchid-icon">
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
-</svg>
-<?php /**PATH /Users/george/Dropbox/code/dine-hub/backend/storage/framework/views/6473cf7069c23d83e48c022ce7bbb1c4.blade.php ENDPATH**/ ?>
