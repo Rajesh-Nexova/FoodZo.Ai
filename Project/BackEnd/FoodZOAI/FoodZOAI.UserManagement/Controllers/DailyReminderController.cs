6c8bf24ecb6ccaf50c87f61c80ffb726cc7cc688
Gokul P <gokulparthi777@gmail.com>
2025-05-21 18:05:30 +0530

    Daily Reminder

---
+++ Project/BackEnd/FoodZOAI/FoodZOAI.UserManagement/Controllers/DailyReminderController.cs
@@ -0,0 +1,57 @@
+﻿using FoodZOAI.UserManagement.Configuration.Contracts;
+using FoodZOAI.UserManagement.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodZOAI.UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DailyReminderController : ControllerBase
+    {
+        private readonly IDailyReminderRepository _dailyReminderRepository;
+        private readonly IDailyReminderMapper _dailyReminderMapper;
+        public DailyReminderController(IDailyReminderRepository dailyReminderRepository, IDailyReminderMapper dailyReminderMapper)
+        {
+            _dailyReminderRepository = dailyReminderRepository;
+            _dailyReminderMapper= dailyReminderMapper;
+
+        }
+
+        //[HttpGet("GetDailyReminders")]
+        //public async Task<IActionResult> GetDailyReminders()
+        //{
+        //    try
+        //    {
+        //        var settings = await _dailyReminderRepository.GetAllAsync();
+        //        var result = _dailyReminderMapper.MapList(settings.ToList());
+        //        return Ok(result);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return StatusCode(500, "An error occurred while retrieving the GetDailyReminders.");
+        //    }
+        //}
+
+        [HttpGet("GetDailyReminders/{id}")]
+        public async Task<IActionResult> GetDailyRemindersById(int id)
+        {
+            try
+            {
+                var setting = await _dailyReminderRepository.GetByIdAsync(id);
+                if (setting == null)
+                    return NotFound("GetDaily Reminders not found.");
+
+                var result = _dailyReminderMapper.MapToDTO(setting);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while retrieving the GetDaily Reminders.");
+            }
+        }
+
+
+
+    }
+}
---
+++ Project/BackEnd/FoodZOAI/FoodZOAI.UserManagement/Controllers/HalfYearlyReminderController.cs
@@ -0,0 +1,38 @@
+﻿using FoodZOAI.UserManagement.Configuration.Contracts;
+using FoodZOAI.UserManagement.Contracts;
+using FoodZOAI.UserManagement.Mappers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodZOAI.UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HalfYearlyReminderController : ControllerBase
+    {
+        private readonly IHalfYearlyReminderRepository _halfYearlyReminderRepository;
+        private readonly IHalfYearlyReminderMapper _halfYearlyReminderMapper;
+        public HalfYearlyReminderController(IHalfYearlyReminderRepository halfYearlyReminderRepository, IHalfYearlyReminderMapper halfYearlyReminderMapper)
+        {
+            _halfYearlyReminderRepository = halfYearlyReminderRepository;
+            _halfYearlyReminderMapper = halfYearlyReminderMapper;
+
+        }
+
+
+        [HttpGet("HalfYearlyReminder/{id}")]
+        public async Task<IActionResult> GetHalfYearlyReminders(int id)
+        {
+            try
+            {
+                var settings = await _halfYearlyReminderRepository.GetAllAsync();
+                var result = _halfYearlyReminderMapper.MapList(settings.ToList());
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while retrieving the HalfYearlyReminder");
+            }
+        }
+    }
+}
